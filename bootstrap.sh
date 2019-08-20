e4794a047ca2582ff702e660efefda3a2680db99
Ian Fosbery <ian.fosbery@reaktor.com>
2019-08-19 22:05:20 -0400

    Update bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,6 +8,7 @@ brew update
 brew tap caskroom/cask
 
 # install me brews -  the obscure ones are probably for asdf
+brew install adns
 brew install ansible
 brew install autoconf
 brew install automake
@@ -15,45 +16,106 @@ brew install aws-elasticbeanstalk
 brew install awscli
 brew install bash-completion
 brew install battery
+brew install circleci
 brew install cmake
-brew install coreutils
+brew install cscope
+brew install digdag
 brew install direnv
 brew install exercism
 brew install fish
 brew install freetype
 brew install gdbm
 brew install gettext
-brew install git
+brew install gmp
+brew install gnu-sed
+brew install gnupg
+brew install gnutls
+brew install go
 brew install grip
 brew install heroku
+brew install heroku-node
+brew install htop
 brew install hub
+brew install icu4c
 brew install imagemagick
+brew install jemalloc
 brew install jpeg
 brew install jq
-brew install kubernetes-cli
-brew install kubetail
+brew install leiningen
+brew install libassuan
+brew install libev
+brew install libevent
+brew install libffi
+brew install libgcrypt
+brew install libgpg-error
+brew install libksba
+brew install libnet
+brew install libpng
+brew install libssh
+brew install libtasn1
+brew install libtermkey
+brew install libtiff
 brew install libtool
-brew install libyaml
+brew install libunistring
+brew install libusb
+brew install libuv
+brew install libvterm
 brew install libxml2
 brew install libxslt
+brew install libyaml
+brew install libzip
+brew install lua
+brew install msgpack
+brew install nettle
+brew install node
+brew install npth
+brew install oniguruma
 brew install openssl
+brew install openssl@1.1
+brew install p11-kit
 brew install parity
-brew tap thoughtbot/formulae
+brew install pcre
+brew install pcre2
+brew install peco
+brew install perform
+brew install perl
+brew install phantomjs
+brew install pinentry
+brew install pkg-config
+brew install postgresql
+brew install python
+brew install qt@5.5
 brew install rcm
 brew install readline
+brew install reattach-to-user-namespace
 brew install ripgrep
+brew install ruby
 brew install siege
+brew install spark
 brew install sqlite
-brew install terraform
+brew install tcptraceroute
+brew install telnet
+brew install themekit
+brew install tldr
 brew install tmate
 brew install tmux
 brew install tree
+brew install truncate
+brew install unibilium
+brew install universal-ctags
 brew install unixodbc
 brew install vim
+brew install watchman
+brew install wxmac
+brew install xz
+brew install yarn
 
 # install casks
 brew cask install 1password
+brew cask install abstract
 brew cask install adobe-reader
+brew cask install airtable
+brew cask install alacritty
 brew cask install brave
 brew cask install docker
 brew cask install emacs
@@ -61,18 +123,18 @@ brew cask install firefox
 brew cask install flux
 brew cask install freedome
 brew cask install front
+brew cask install google-drive-file-stream
 brew cask install google-chrome
 brew cask install grammarly
+brew cask install graphiql
 brew cask install insomnia
 brew cask install intellij-idea-ce
 brew cask install iterm2
 brew cask install java
 brew cask install keycastr
 brew cask install kindle
-brew cask install libreoffice
 brew cask install licecap
 brew cask install macdown
-brew cask install pencil
 brew cask install postgres
 brew cask install sequel-pro
 brew cask install sketch
@@ -85,6 +147,8 @@ brew cask install sublime-text
 brew cask install terraform
 brew cask install vagrant
 brew cask install virtualbox
+brew cask install visual-studio-code
+brew cask install vlc
 brew cask install zeplin
 
 # install ctags after emacs
