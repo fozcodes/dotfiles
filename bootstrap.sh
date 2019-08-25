f2423e6615bfc99c37fed5c634abb494310e56b3
Ian Fosbery <ian.fosbery@reaktor.com>
2019-08-24 22:02:41 -0400

    Update fish theme

--- bootstrap.sh
+++ bootstrap.sh
@@ -172,6 +172,9 @@ asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
 # setup fish
 echo "/usr/local/bin/fish" | sudo tee -a /etc/shells
 chsh -s /usr/local/bin/fish
+curl -L https://get.oh-my.fish | fish
+omf install agnoster
+omf theme agnoster
 
 # copy rcm config
 ln -s ~/.dotfiles/.rcrc ~/.rcrc
