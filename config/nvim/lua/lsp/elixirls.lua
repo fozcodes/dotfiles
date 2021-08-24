7be520688f64f4baadefbfcacab02354a6c5cfa8
Ian Fosbery <ian.fosbery@reaktor.com>
2021-08-24 19:18:47 -0400

    Add elixir, lua, vue lsps

---
+++ config/nvim/lua/lsp/elixirls.lua
@@ -0,0 +1,44 @@
+local nvim_lsp = require("lspconfig")
+
+local path_to_elixirls = vim.fn.expand("~/.elixir/elixir-ls/rel/language_server.sh")
+
+local M = {}
+M.setup = function(on_attach)
+
+  -- These have a different style than above because I was fiddling
+  -- around and never converted them. Instead of converting them
+  -- now, I'm leaving them as they are for this article because this is
+  -- what I actually use, and hey, it works ¯\_(ツ)_/¯.
+  vim.cmd [[imap <expr> <C-l> vsnip#available(1) ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>']]
+  vim.cmd [[smap <expr> <C-l> vsnip#available(1) ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>']]
+
+  vim.cmd [[imap <expr> <Tab> vsnip#jumpable(1) ? '<Plug>(vsnip-jump-next)' : '<Tab>']]
+  vim.cmd [[smap <expr> <Tab> vsnip#jumpable(1) ? '<Plug>(vsnip-jump-next)' : '<Tab>']]
+  vim.cmd [[imap <expr> <S-Tab> vsnip#jumpable(-1) ? '<Plug>(vsnip-jump-prev)' : '<S-Tab>']]
+  vim.cmd [[smap <expr> <S-Tab> vsnip#jumpable(-1) ? '<Plug>(vsnip-jump-prev)' : '<S-Tab>']]
+
+  vim.cmd [[inoremap <silent><expr> <C-Space> compe#complete()]]
+  vim.cmd [[inoremap <silent><expr> <CR> compe#confirm('<CR>')]]
+  vim.cmd [[inoremap <silent><expr> <C-e> compe#close('<C-e>')]]
+  vim.cmd [[inoremap <silent><expr> <C-f> compe#scroll({ 'delta': +4 })]]
+  vim.cmd [[inoremap <silent><expr> <C-d> compe#scroll({ 'delta': -4 })]]
+
+  nvim_lsp.elixirls.setup({
+    cmd = { path_to_elixirls },
+    on_attach = function(client, bufnr)
+      client.resolved_capabilities.document_formatting = false
+
+      on_attach(client, bufnr)
+    end;
+    settings = {
+      elixirLS = {
+        dialyzerEnabled = true,
+        suggestSpecs = true,
+        enableTestLenses = true
+      }
+    }
+  })
+
+end
+
+return M
--- config/nvim/lua/lsp/init.lua
+++ config/nvim/lua/lsp/init.lua
@@ -1,6 +1,12 @@
 local u = require("utils")
 local null_ls = require("lsp.null-ls")
 local tsserver = require("lsp.tsserver")
+local elixir = require("lsp.elixirls")
+local efm = require("lsp.efm")
+local pyls = require("lsp.pylspserver")
+local vue = require("lsp.vue")
+local lua = require("lsp.lualsp")
+
 
 -- https://neovim.io/doc/user/api.html#nvim_open_win()
 local popup_opts = { border = "rounded", focusable = false, margin = {10,10,10,10}}
@@ -14,6 +20,8 @@ local on_attach = function(client, bufnr)
     u.lua_command("LspFormatting", "vim.lsp.buf.formatting()")
     u.lua_command("LspHover", "vim.lsp.buf.hover()")
     u.lua_command("LspRename", "vim.lsp.buf.rename()")
+    u.lua_command("LspDef", "vim.lsp.buf.definition()")
+    u.lua_command("LspDecl", "vim.lsp.buf.declaration()")
     u.lua_command("LspDiagPrev", "global.lsp.prev_diagnostic()")
     u.lua_command("LspDiagNext", "global.lsp.next_diagnostic()")
     u.lua_command("LspDiagLine", "vim.lsp.diagnostic.show_line_diagnostics(global.lsp.popup_opts)")
@@ -23,22 +31,24 @@ local on_attach = function(client, bufnr)
 
     local opts = { noremap=true, silent=true }
     -- bindings
-    u.buf_map("n", "gd", "<Cmd>lua vim.lsp.buf.definition()<CR>", opts, bufnr)
-    u.buf_map("n", "gD", "<Cmd>lua vim.lsp.buf.declaration()<CR>", opts, bufnr)
+    u.buf_map("n", "gd", ":LspDef<CR>", opts, bufnr)
+    u.buf_map("n", "gD", ":LspDecl<CR>", opts, bufnr)
     u.buf_map("n", "gi", ":LspRename<CR>", nil, bufnr)
     u.buf_map("n", "K", ":LspHover<CR>", nil, bufnr)
     u.buf_map("n", "[a", ":LspDiagPrev<CR>", nil, bufnr)
     u.buf_map("n", "]a", ":LspDiagNext<CR>", nil, bufnr)
     u.buf_map("i", "<C-x><C-x>", "<cmd> LspSignatureHelp<CR>", nil, bufnr)
 
+    print(vim.lsp.names)
+
     vim.lsp.handlers["textDocument/hover"] =  vim.lsp.with(vim.lsp.handlers.hover, {border = "rounded", margin = {1,10,1,10}})
-    vim.lsp.handlers["textDocument/signatureHelp"] =  vim.lsp.with(vim.lsp.handlers.signature_help, {border = border})
+    vim.lsp.handlers["textDocument/signatureHelp"] =  vim.lsp.with(vim.lsp.handlers.signature_help, {border = "rounded"})
     -- Automatically update diagnostics ... from
     -- https://github.com/folke/dot/blob/master/config/nvim/lua/config/lsp/diagnostics.lua
     vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
             underline = true,
             update_in_insert = false,
-            virtual_text = { spacing = 4, prefix = "●" },
+            virtual_text = { spacing = 4, prefix = string.gsub("● $name: ", "%$(%w+)", {name=client.name}) },
             severity_sort = true,
         })
 
@@ -48,10 +58,11 @@ local on_attach = function(client, bufnr)
         local hl = "LspDiagnosticsSign" .. type
         vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
     end
-
-    if client.resolved_capabilities.document_formatting then
-        u.buf_augroup("LspFormatOnSave", "BufWritePre", "lua vim.lsp.buf.formatting_sync()")
-    end
 end
 
 tsserver.setup(on_attach)
+elixir.setup(on_attach)
+efm.setup(on_attach)
+pyls.setup(on_attach)
+vue.setup(on_attach)
+lua.setup(on_attach)
