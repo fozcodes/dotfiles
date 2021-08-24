ffe57cebcd0346c1875d590ddc563f96ab81ff34
Ian Fosbery <ian.fosbery@reaktor.com>
2021-08-24 19:17:36 -0400

    Add EFM server for elixir formatting

---
+++ config/nvim/lua/lsp/efm.lua
@@ -0,0 +1,33 @@
+local nvim_lsp = require("lspconfig")
+
+-- efm-langserver language modules--
+-- mixformat | testing only --
+--local mixformat = {
+    --formatCommand = 'mix format -',
+    --formatStdin = true
+  --}
+
+-- efm-langserver languages settings --
+--local efm_languages = {
+  --elixir = { mixformat }
+--}
+
+-- Filetypes supported --
+local efm_filetypes = {"elixir"}
+
+local home = os.getenv("HOME")
+
+local M = {}
+M.setup = function(on_attach)
+    nvim_lsp.efm.setup{
+        init_options = { documentFormatting = true },
+        on_attach = function(client, bufnr)
+            vim.api.nvim_command("au BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()")
+
+            on_attach(client, bufnr)
+        end;
+        filetypes = efm_filetypes
+    }
+end
+
+return M
