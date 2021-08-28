fbb621baa2ddea18360a15b03ad6d2a344a372a9
Ian Fosbery <ian.fosbery@reaktor.com>
2021-08-28 12:44:46 -0400

    Move EFM config into lua

--- config/nvim/lua/lsp/efm.lua
+++ config/nvim/lua/lsp/efm.lua
@@ -13,7 +13,16 @@ local nvim_lsp = require("lspconfig")
 -- }
 
 -- Filetypes supported --
-local efm_filetypes = {"elixir", "typescript", "typescriptreact", "javascript", "yaml", "json", "html", "scss", "css", "markdown", "lua"}
+local efm_filetypes = {
+    "elixir", "typescript", "typescriptreact", "javascript", "yaml", "json", "html", "scss", "css", "markdown", "lua"
+}
+
+local prettier_format_command = {formatCommand = "prettierd ${INPUT}", formatStdin = true}
+local eslint_lint_command = {
+    lintCommand = "eslint_d -f unix --stdin --stdin-filename ${INPUT}",
+    lintStdin = true,
+    lintIgnoreExitCode = true
+}
 
 local M = {}
 M.setup = function(on_attach)
@@ -24,7 +33,38 @@ M.setup = function(on_attach)
 
             on_attach(client, bufnr)
         end,
-        filetypes = efm_filetypes
+        filetypes = efm_filetypes,
+        settings = {
+            rootMarkers = {".git/"},
+            lintDebounce = "0.3s",
+            languages = {
+                elixir = {
+                    {formatCommand = "mix format - ${INPUT}", formatStdin = true}, {
+                        lintCommand = "MIX_ENV=test mix credo suggest --format=flycheck --read-from-stdin ${INPUT}",
+                        lintStdin = true,
+                        lintIgnoreExitCode = true,
+                        lintFormats = {'%f:%l:%c: %t: %m', '%f:%l: %t: %m'},
+                        rootMarkers = {"mix.lock", "mix.exs"}
+                    }
+                },
+                lua = {
+                    {
+                        formatCommand = "lua-format -i --no-keep-simple-function-one-line --no-break-after-operator --column-limit=120 --break-after-table-lb",
+                        formatStdin = true
+                    }
+                },
+                typescript = {prettier_format_command, eslint_lint_command},
+                javascript = {prettier_format_command, eslint_lint_command},
+                typescriptreact = {prettier_format_command, eslint_lint_command},
+                javascriptreact = {prettier_format_command, eslint_lint_command},
+                json = {prettier_format_command},
+                yaml = {prettier_format_command},
+                html = {prettier_format_command},
+                scss = {prettier_format_command},
+                css = {prettier_format_command},
+                markdown = {prettier_format_command}
+            }
+        }
     })
 end
 
