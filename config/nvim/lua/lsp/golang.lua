9d2c87ec3dc9465be317dbde3ec124f27142f5b5
Ian Fosbery <ian.fosbery@reaktor.com>
2022-04-09 12:26:35 -0400

    Add golang lsp config

---
+++ config/nvim/lua/lsp/golang.lua
@@ -0,0 +1,27 @@
+local nvim_lsp = require("lspconfig")
+
+local M = {}
+M.setup = function(on_attach, capabilities)
+
+    nvim_lsp.gopls.setup {
+        cmd = {'gopls'},
+        on_attach = function(client, bufnr)
+            client.resolved_capabilities.document_formatting = true
+            vim.cmd [[autocmd BufWritePre <buffer> lua vim.lsp.buf.formatting_sync()]]
+            on_attach(client, bufnr)
+        end,
+        capabilities = capabilities,
+        settings = {
+            gopls = {
+                experimentalPostfixCompletions = true,
+                analyses = {unusedparams = true, shadow = true},
+                staticcheck = true
+            }
+        },
+        filetypes = {"go"}
+    }
+
+end
+
+return M
+
